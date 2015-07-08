61994ffc401547a0d689012aa8e19e8e3010d6cc
Gregory Amerson <gregory.amerson@liferay.com>
2015-07-09 02:10:55 +0800

    [migrator] test update fixes #22

--- blade.migrate.test/src/blade/migrate/test/AllProblemsTest.java
+++ blade.migrate.test/src/blade/migrate/test/AllProblemsTest.java
@@ -26,7 +26,7 @@ public class AllProblemsTest {
 				.findProblems(new File(
 						"../blade.migrate.liferay70/projects/"));
 
-		assertEquals(66, problems.size());
+		assertEquals(72, problems.size());
 	}
 
 	private final BundleContext context = FrameworkUtil.getBundle(
