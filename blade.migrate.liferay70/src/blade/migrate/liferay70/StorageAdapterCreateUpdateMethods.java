081a645dce4542d2a0a64610c9a3f9ab77fe271b
Gregory Amerson <gregory.amerson@liferay.com>
2015-07-14 23:17:08 +0800

    source formatting

--- blade.migrate.liferay70/src/blade/migrate/liferay70/StorageAdapterCreateUpdateMethods.java
+++ blade.migrate.liferay70/src/blade/migrate/liferay70/StorageAdapterCreateUpdateMethods.java
@@ -14,9 +14,10 @@ import blade.migrate.core.SearchResult;
 @Component(
 	property = {
 		"file.extension=java",
+		"problem.title=StorageAdapter API Changes",
 		"problem.summary=Removed Operations That Used the Fields Class from the StorageAdapter Interface",
 		"problem.tickets=LPS-53021",
-		"problem.title=StorageAdapter API Changes", "problem.type=java",
+		"problem.type=java",
 		"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#removed-operations-that-used-the-fields-class-from-the-storageadapter-interface"
 	},
 	service = FileMigrator.class
@@ -28,17 +29,20 @@ public class StorageAdapterCreateUpdateMethods extends JavaFileMigrator {
 		final List<SearchResult> searchResults = new ArrayList<>();
 		final JavaFileChecker javaFileChecker = new JavaFileChecker(file);
 
-		List<SearchResult> invocations = javaFileChecker.findMethodInvocations(null, "StorageEngineUtil", "create",
-				new String[] { "long", "long", "Fields" ,"ServiceContext"});
+		List<SearchResult> invocations = javaFileChecker.findMethodInvocations(
+				null, "StorageEngineUtil", "create",
+				new String[] { "long", "long", "Fields", "ServiceContext" });
 
 		searchResults.addAll(invocations);
 
-		invocations = javaFileChecker.findMethodInvocations(null, "StorageEngineUtil", "update",
+		invocations = javaFileChecker.findMethodInvocations(null,
+				"StorageEngineUtil", "update",
 				new String[] { "long", "Fields", "boolean", "ServiceContext" });
 
 		searchResults.addAll(invocations);
 
-		invocations = javaFileChecker.findMethodInvocations(null, "StorageEngineUtil", "update",
+		invocations = javaFileChecker.findMethodInvocations(null,
+				"StorageEngineUtil", "update",
 				new String[] { "long", "Fields", "ServiceContext" });
 
 		searchResults.addAll(invocations);
