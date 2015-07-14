4865b4eac6e3a50dce554d9fe21b7858ef69a0d5
Gregory Amerson <gregory.amerson@liferay.com>
2015-07-14 23:52:29 +0800

    [migrator] source formatting and test update

--- blade.migrate.liferay70/src/blade/migrate/liferay70/ScreenNameValidatorInterface.java
+++ blade.migrate.liferay70/src/blade/migrate/liferay70/ScreenNameValidatorInterface.java
@@ -1,7 +1,6 @@
 package blade.migrate.liferay70;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
@@ -12,20 +11,21 @@ import blade.migrate.core.JavaFileMigrator;
 import blade.migrate.core.SearchResult;
 
 @Component(
-		property = {
-			"file.extensions=java",
-			"problem.title=Added New Methods in the ScreenNameValidator Interface",
-			"problem.summary=The ScreenNameValidator interface has new methods getDescription(Locale) and getJSValidation().",
-			"problem.tickets=LPS-53409",
-			"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#added-new-methods-in-the-screennamevalidator-interface"
-		},
-		service = FileMigrator.class
-	)
-
+	property = {
+		"file.extensions=java",
+		"problem.title=Added New Methods in the ScreenNameValidator Interface",
+		"problem.summary=The ScreenNameValidator interface has new methods getDescription(Locale) and getJSValidation().",
+		"problem.tickets=LPS-53409",
+		"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#added-new-methods-in-the-screennamevalidator-interface"
+	},
+	service = FileMigrator.class
+)
 public class ScreenNameValidatorInterface extends JavaFileMigrator {
+
 	@Override
 	protected List<SearchResult> searchJavaFile(File file) {
 		final JavaFileChecker javaFileChecker = new JavaFileChecker(file);
+
 		return javaFileChecker.findImplementsInterface("ScreenNameValidator");
 	}
 }
--- blade.migrate.test/src/blade/migrate/test/AllProblemsTest.java
+++ blade.migrate.test/src/blade/migrate/test/AllProblemsTest.java
@@ -26,7 +26,7 @@ public class AllProblemsTest {
 				.findProblems(new File(
 						"../blade.migrate.liferay70/projects/"));
 
-		assertEquals(78, problems.size());
+		assertEquals(79, problems.size());
 	}
 
 	private final BundleContext context = FrameworkUtil.getBundle(
