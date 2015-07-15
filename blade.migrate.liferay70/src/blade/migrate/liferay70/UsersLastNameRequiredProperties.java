c7dda7a07a9abd678b1307ea4b5e6c88f749cd28
Gregory Amerson <gregory.amerson@liferay.com>
2015-07-15 11:32:16 +0800

    source formatting

--- blade.migrate.liferay70/src/blade/migrate/liferay70/UsersLastNameRequiredProperties.java
+++ blade.migrate.liferay70/src/blade/migrate/liferay70/UsersLastNameRequiredProperties.java
@@ -6,18 +6,18 @@ import org.osgi.service.component.annotations.Component;
 
 import blade.migrate.api.FileMigrator;
 @Component(
-		property = {
-			"file.extensions=properties",
-			"problem.title=Removed USERS_LAST_NAME_REQUIRED from portal.properties in Favor of language.properties Configurations",
-			"problem.summary=The USERS_LAST_NAME_REQUIRED property has been removed "
-											+ "from portal.properties and the corresponding UI. Required names are now handled "
-											+ "on a per-language basis via the language.properties files. It has also been removed as "
-											+ "an option from the Portal Settings section of the Control Panel.",
-			"problem.tickets=LPS-54956",
-			"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#removed-users_last_name_required-from-portalproperties-in-favor-of-languageproperties-configurations",
-		},
-		service = FileMigrator.class
-	)
+	property = {
+		"file.extensions=properties",
+		"problem.title=Removed USERS_LAST_NAME_REQUIRED from portal.properties",
+		"problem.summary=The USERS_LAST_NAME_REQUIRED property has been removed "
+				+ "from portal.properties and the corresponding UI. Required names are now handled "
+				+ "on a per-language basis via the language.properties files. It has also been removed as "
+				+ "an option from the Portal Settings section of the Control Panel.",
+		"problem.tickets=LPS-54956",
+		"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#removed-users_last_name_required-from-portalproperties-in-favor-of-languageproperties-configurations",
+	},
+	service = FileMigrator.class
+)
 public class UsersLastNameRequiredProperties extends PropertiesFileMigrator {
 
 	@Override
