9db5793cbc6aaa7fbe22ab20c9c2b1f1b1712630
Gregory Amerson <gregory.amerson@liferay.com>
2015-07-15 11:40:50 +0800

    source formatting

--- blade.migrate.liferay70/src/blade/migrate/liferay70/ConvertProcessExtends.java
+++ blade.migrate.liferay70/src/blade/migrate/liferay70/ConvertProcessExtends.java
@@ -11,24 +11,24 @@ import blade.migrate.core.JavaFileMigrator;
 import blade.migrate.core.SearchResult;
 
 @Component(
-		property = {
-			"file.extensions=java",
-			"problem.title=Convert Process Classes Are No Longer Specified via the convert.processes Portal Property, but Are Contributed as OSGi Modules",
-			"problem.summary=The implementation class com.liferay.portal.convert.ConvertProcess was renamed com.liferay.portal.convert.BaseConvertProcess. "
-                                            + "An interface named com.liferay.portal.convert.ConvertProcess was created for it."
-                                            + " The convert.processes key was removed from portal.properties. "
-                                            + "Consequentially, ConvertProcess implementations must register as OSGi components.",
-			"problem.tickets=LPS-50604",
-			"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#convert-process-classes-are-no-longer-specified-via-the-convertprocesses-portal-property-but-are-contributed-as-osgi-modules"
-		},
-		service = FileMigrator.class
-	)
-
+	property = {
+		"file.extensions=java",
+		"problem.title=Convert Process Classes don't support convert.processes Portal Property",
+		"problem.summary=The implementation class com.liferay.portal.convert.ConvertProcess was renamed com.liferay.portal.convert.BaseConvertProcess. "
+                + "An interface named com.liferay.portal.convert.ConvertProcess was created for it."
+                + " The convert.processes key was removed from portal.properties. "
+                + "Consequentially, ConvertProcess implementations must register as OSGi components.",
+		"problem.tickets=LPS-50604",
+		"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#convert-process-classes-are-no-longer-specified-via-the-convertprocesses-portal-property-but-are-contributed-as-osgi-modules"
+	},
+	service = FileMigrator.class
+)
 public class ConvertProcessExtends  extends JavaFileMigrator {
 
 	@Override
 	protected List<SearchResult> searchJavaFile(File file) {
 		final JavaFileChecker javaFileChecker = new JavaFileChecker(file);
+
 		return  javaFileChecker.findSuperClass("ConvertProcess");
 	}
 }
--- blade.migrate.liferay70/src/blade/migrate/liferay70/ConvertProcessProperties.java
+++ blade.migrate.liferay70/src/blade/migrate/liferay70/ConvertProcessProperties.java
@@ -9,11 +9,11 @@ import blade.migrate.api.FileMigrator;
 @Component(
 	property = {
 		"file.extensions=properties",
-		"problem.title=Convert Process Classes Are No Longer Specified via the convert.processes Portal Property, but Are Contributed as OSGi Modules",
+		"problem.title=Convert Process Classes don't support convert.processes Portal Property",
 		"problem.summary=The implementation class com.liferay.portal.convert.ConvertProcess was renamed com.liferay.portal.convert.BaseConvertProcess. "
-                                        + "An interface named com.liferay.portal.convert.ConvertProcess was created for it."
-                                        + " The convert.processes key was removed from portal.properties. "
-                                        + "Consequentially, ConvertProcess implementations must register as OSGi components.",
+                + "An interface named com.liferay.portal.convert.ConvertProcess was created for it."
+                + " The convert.processes key was removed from portal.properties. "
+                + "Consequentially, ConvertProcess implementations must register as OSGi components.",
 		"problem.tickets=LPS-50604",
 		"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#convert-process-classes-are-no-longer-specified-via-the-convertprocesses-portal-property-but-are-contributed-as-osgi-modules"
 	},
