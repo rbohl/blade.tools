3b71f05efe1eaebf98f6a141be8c85ecdceacc28
Gregory Amerson <gregory.amerson@liferay.com>
2015-07-16 22:20:04 +0800

    source formatting

--- blade.migrate.liferay70/src/blade/migrate/liferay70/ReservedUserIdExceptionCatch.java
+++ blade.migrate.liferay70/src/blade/migrate/liferay70/ReservedUserIdExceptionCatch.java
@@ -11,23 +11,21 @@ import blade.migrate.core.JavaFileMigrator;
 import blade.migrate.core.SearchResult;
 
 @Component(
-		property = {
-			"file.extensions=java",
-			"problem.title=Replaced ReservedUserIdException with UserIdException Inner Classes",
-			"problem.summary=The ReservedUserIdException has been deprecated and replaced with UserIdException.MustNotBeReserved.",
-			"problem.tickets=LPS-53487",
-			"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#replaced-reserveduseridexception-with-useridexception-inner-classes"
-		},
-		service = FileMigrator.class
-	)
-
+	property = {
+		"file.extensions=java",
+		"problem.title=Replaced ReservedUserIdException with UserIdException Inner Classes",
+		"problem.summary=The ReservedUserIdException has been deprecated and replaced with UserIdException.MustNotBeReserved.",
+		"problem.tickets=LPS-53487",
+		"problem.url=https://github.com/liferay/liferay-portal/blob/master/readme/7.0/BREAKING_CHANGES.markdown#replaced-reserveduseridexception-with-useridexception-inner-classes"
+	},
+	service = FileMigrator.class
+)
 public class ReservedUserIdExceptionCatch  extends JavaFileMigrator {
 
 	@Override
 	protected List<SearchResult> searchJavaFile(File file) {
 		final JavaFileChecker javaFileChecker = new JavaFileChecker(file);
-		List<SearchResult> results = javaFileChecker.findCatchExceptions(new String[]{"ReservedUserIdException"});
-		System.out.println(results);
-		return results;
+
+		return javaFileChecker.findCatchExceptions(new String[]{"ReservedUserIdException"});
 	}
 }
