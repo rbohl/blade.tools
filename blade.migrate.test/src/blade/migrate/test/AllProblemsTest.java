39a921bf9cf9528b34b93fee574eaecf1488bcaf
Gregory Amerson <gregory.amerson@liferay.com>
2015-07-22 16:52:09 -0500

    Total problem count update

--- blade.migrate.test/src/blade/migrate/test/AllProblemsTest.java
+++ blade.migrate.test/src/blade/migrate/test/AllProblemsTest.java
@@ -24,7 +24,7 @@ public class AllProblemsTest {
 				.findProblems(new File(
 						"../blade.migrate.liferay70/projects/"));
 
-		assertEquals(116, problems.size());
+		assertEquals(126, problems.size());
 	}
 
 	private final BundleContext context = FrameworkUtil.getBundle(
