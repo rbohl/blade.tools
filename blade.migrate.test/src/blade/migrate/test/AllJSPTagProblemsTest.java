9a1160ff7f6e2dc752f940aecff9f3be74c4f51e
Gregory Amerson <gregory.amerson@liferay.com>
2015-09-17 16:27:30 -0500

    [migrator] create a separate test file for all JSP tests

---
+++ blade.migrate.test/src/blade/migrate/test/AllJSPTagProblemsTest.java
@@ -0,0 +1,38 @@
+package blade.migrate.test;
+
+import static org.junit.Assert.assertEquals;
+
+import blade.migrate.api.Migration;
+import blade.migrate.api.Problem;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+
+public class AllJSPTagProblemsTest {
+
+	@Test
+	public void allProblems() throws Exception {
+		ServiceReference<Migration> sr = context
+			.getServiceReference(Migration.class);
+		Migration m = context.getService(sr);
+		List<Problem> problems = m.findProblems(new File("jsptests/"));
+
+		final int expectedSize = 22;
+		final int size = problems.size();
+
+		if (size != expectedSize) {
+			System.err.println("All problems size is " + size + ", expected size is " + expectedSize);
+		}
+
+		assertEquals(expectedSize, size);
+	}
+
+	private final BundleContext context = FrameworkUtil.getBundle(
+		this.getClass()).getBundleContext();
+
+}
