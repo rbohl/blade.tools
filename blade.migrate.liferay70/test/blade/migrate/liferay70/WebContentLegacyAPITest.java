253af6eb93eadcac4f315bfa93cb6b52b72128f8
Gregory Amerson <gregory.amerson@liferay.com>
2015-09-16 19:38:01 -0500

    [migrator] add check for all Web Content APIs fixes #210

---
+++ blade.migrate.liferay70/test/blade/migrate/liferay70/WebContentLegacyAPITest.java
@@ -0,0 +1,60 @@
+package blade.migrate.liferay70;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import blade.migrate.core.JavaFileChecker;
+import blade.migrate.core.SearchResult;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebContentLegacyAPITest {
+	final File testFile = new File(
+			"projects/legacy-apis-ant-portlet/docroot/WEB-INF/src/com/liferay/LegacyAPIsAntPortlet.java");
+	WebContentLegacyAPI component;
+
+	@Before
+	public void beforeTest() {
+		assertTrue(testFile.exists());
+		component = new WebContentLegacyAPI();
+	}
+
+	@Test
+	public void webContentLegacyAPITest() throws Exception {
+		List<SearchResult> results = component.searchJavaFile(testFile,
+				new JavaFileChecker(testFile));
+
+		assertNotNull(results);
+		assertEquals(4, results.size());
+
+		SearchResult problem = results.get(0);
+
+		assertEquals(20, problem.startLine);
+		assertEquals(942, problem.startOffset);
+		assertEquals(1004, problem.endOffset);
+
+		problem = results.get(1);
+
+		assertEquals(21, problem.startLine);
+		assertEquals(1013, problem.startOffset);
+		assertEquals(1079, problem.endOffset);
+
+		problem = results.get(2);
+
+		assertEquals(41, problem.startLine);
+		assertEquals(1597, problem.startOffset);
+		assertEquals(1655, problem.endOffset);
+
+		problem = results.get(3);
+
+		assertEquals(45, problem.startLine);
+		assertEquals(1786, problem.startOffset);
+		assertEquals(1829, problem.endOffset);
+	}
+
+}
