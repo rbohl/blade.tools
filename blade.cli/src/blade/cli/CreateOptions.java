307c03d10e5b6dff6e7eb6c7ea78b98ab30df6ec
Russell Bohl <russell.bohl@liferay.com>
2015-09-22 11:39:37 -0400

    Added a serviceWrapper Gradle template

--- blade.cli/src/blade/cli/CreateOptions.java
+++ blade.cli/src/blade/cli/CreateOptions.java
@@ -33,7 +33,7 @@ public interface CreateOptions extends Options {
 	public IDE ide();
 
 	@Description("The type of Liferay module to create. "
-            + "Valid values are portlet, jspportlet, or service.")
+            + "Valid values are portlet, jspportlet, service, or serviceWrapper.")
 	public Type projectType();
 
 	@Description("The version of Liferay to create the module for, "
